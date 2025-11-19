8cfbe46e021f7335be3fb7801f081fb635c0d6b2
mohamed blemili <rrarife7@gmail.com>
2025-11-19 12:31:33 +0100

    initial commit

---
+++ pw7/src/main/java/com/example/pw7/Pw7Application.java
@@ -0,0 +1,13 @@
+package com.example.pw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw7Application.class, args);
+	}
+
+}
---
+++ pw7/src/test/java/com/example/pw7/Pw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pw7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
